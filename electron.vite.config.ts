bc01f6e747a42523598f47fe50e5ac126fecfd38
cour <15732964521@163.com>
2024-12-05 13:06:27 +0800

    feat✨: 简单修改打包参数

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,51 @@
 import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin, loadEnv } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+export default defineConfig(({ command, mode }) => {
+  console.log(`当前运行命令：${command}`)
+  console.log(`当前运行模式：${mode}`)
+  const env = loadEnv(mode)
+  console.log(`当前运行模式：${env}`)
+  const IS_DEV = mode == 'development'
+  //  gzip 压缩大小报告
+  const IS_GZIP_REPORT = IS_DEV ? false : true
+  //资源文件夹
+  const RESOURCES_DIR = 'resources'
+  // 通用编译参数
+  const buildGeneral = {
+    publicDir: RESOURCES_DIR,
+    reportCompressedSize: IS_GZIP_REPORT
+  }
+  return {
+    main: {
+      build: {
+        envPrefix: 'RYM_',
+        outDir: 'out/main',
+        ...buildGeneral
+      },
+      plugins: [externalizeDepsPlugin()]
     },
-    plugins: [vue()]
+    preload: {
+      build: {
+        envPrefix: 'RYP_',
+        outDir: 'out/preload',
+        ...buildGeneral
+      },
+      plugins: [externalizeDepsPlugin()]
+    },
+    renderer: {
+      build: {
+        envPrefix: 'RYW_',
+        outDir: 'out/renderer',
+        ...buildGeneral
+      },
+      resolve: {
+        alias: {
+          '@renderer': resolve('src/renderer/src')
+        }
+      },
+      plugins: [vue()]
+    }
   }
 })
